8f3980ba053d2f213e36049cdfed330e1e6bbc80
Cody W Tucker <cody@tmvsocial.com>
2025-04-17 10:07:40 -0500

    update nix-flake to uv

--- packaging/flake.nix
+++ packaging/flake.nix
@@ -3,44 +3,56 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.poetry2nix = {
-    url = "github:nix-community/poetry2nix";
-    inputs.nixpkgs.follows = "nixpkgs";
-    inputs.flake-utils.follows = "flake-utils";
-  };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    poetry2nix,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      p2n = import poetry2nix {inherit pkgs;};
-      python = pkgs.python3;
-      projectDir = ../.;
-      overrides = p2n.overrides.withDefaults (final: prev: {
-        # use wheels to build ruff & mypy
-        ruff = prev.ruff.override {
-          preferWheel = true;
-        };
-        mypy = prev.mypy.override {
-          preferWheel = true;
-        };
-      });
-      poetry_env = p2n.mkPoetryEnv {
-        inherit python projectDir overrides;
-      };
-      poetry_app = p2n.mkPoetryApplication {
-        inherit python projectDir overrides;
-      };
       pkgs = nixpkgs.legacyPackages.${system};
+      python = pkgs.python3;
+      pythonEnv = python.withPackages (ps: with ps; [
+        python-dateutil
+      ]);
     in {
       packages = {
-        nextmeeting = poetry_app;
+        nextmeeting = pkgs.python3Packages.buildPythonPackage {
+          pname = "nextmeeting";
+          version = "1.5.5";
+          src = ../.;
+          propagatedBuildInputs = [ pythonEnv ];
+          pythonImportsCheck = [ "nextmeeting" ];
+          format = "pyproject";
+          nativeBuildInputs = [
+            pkgs.python3Packages.hatchling
+          ];
+          # Add development tools to the package
+          checkInputs = [
+            pkgs.ruff
+            pkgs.python3Packages.mypy
+          ];
+          # Configure wheel preferences for development tools
+          preBuildPhases = [ "preferWheelPhase" ];
+          preferWheelPhase = ''
+            export PIP_PREFER_BINARY=1
+          '';
+        };
         default = self.packages.${system}.nextmeeting;
       };
-      devShells.default =
-        pkgs.mkShell {packages = [pkgs.poetry poetry_env];};
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.uv
+          pythonEnv
+          # Development tools
+          pkgs.ruff
+          pkgs.python3Packages.mypy
+        ];
+        shellHook = ''
+          export PYTHONPATH="$PWD:$PYTHONPATH"
+          # Configure wheel preferences
+          export PIP_PREFER_BINARY=1
+        '';
+      };
     });
 }
